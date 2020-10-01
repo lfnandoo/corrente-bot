954c0c3169f0d79ab72fb2979768607f787df895
lfnandoo <devfernandoof@gmail.com>
2020-10-01 11:42:40 -0300

    feat: feedback logs into console

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const prompt = require("prompt-sync")();
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const user = prompt("User profile URL (https:/www.lindekin.com/in/xxx): ");
   await page.setViewport({ width: 100, height: 1000 });
+
+  const user = prompt("User profile URL (https:/www.lindekin.com/in/xxx): ");
   await page.goto(
     `https://www.linkedin.com/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fin%2F${user}&fromSignIn=true&trk=public_authwall_profile-login-link`
   );
@@ -15,15 +16,20 @@ const prompt = require("prompt-sync")();
   const password = prompt("Linkedin password: ");
   await page.type("#password", password);
   await page.focus("button");
+  console.log("======== Login ========");
   await page.$eval("button", (form) => form.click());
 
   await page.waitForNavigation();
   await page.waitForSelector(".pv-top-card");
   const div = await page.$(".pv-top-card");
+
+  console.log("======== Capturing screenshot ========");
   await div.screenshot({
     path: "fer.png",
     clip: { x: 0, y: 170, width: 300, height: 400 }
   });
+  console.log("======== Screenshot saved ========");
 
+  console.log("======== Log off ========");
   await browser.close();
 })();
