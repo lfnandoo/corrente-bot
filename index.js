45cd30094f3df41dc2714cf23e56b041c30abc4a
lfnandoo <devfernandoof@gmail.com>
2020-10-01 12:08:50 -0300

    feat: function getScreenshot

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 
+async function getScreenshot(page) {
+  const user = prompt("User profile URL (https:/www.lindekin.com/in/xxx): ");
+  await page.goto(`https://www.linkedin.com/in/${user}`);
+
+  await page.waitForSelector(".pv-top-card");
+  const div = await page.$(".pv-top-card");
+
+  console.log("======== Capturing screenshot ========");
+  await div.screenshot({
+    path: `${user}.png`,
+    clip: { x: 0, y: 170, width: 300, height: 400 }
+  });
+  console.log("======== Screenshot saved ========");
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 100, height: 1000 });
 
-  const user = prompt("User profile URL (https:/www.lindekin.com/in/xxx): ");
   await page.goto(
-    `https://www.linkedin.com/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fin%2F${user}&fromSignIn=true&trk=public_authwall_profile-login-link`
+    `https://www.linkedin.com/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fin%2Fluizfernandoo&fromSignIn=true&trk=public_authwall_profile-login-link`
   );
 
   const userNameOrEmail = prompt("Linkedin username or email: ");
@@ -20,15 +34,7 @@ const prompt = require("prompt-sync")();
   await page.$eval("button", (form) => form.click());
 
   await page.waitForNavigation();
-  await page.waitForSelector(".pv-top-card");
-  const div = await page.$(".pv-top-card");
-
-  console.log("======== Capturing screenshot ========");
-  await div.screenshot({
-    path: "fer.png",
-    clip: { x: 0, y: 170, width: 300, height: 400 }
-  });
-  console.log("======== Screenshot saved ========");
+  await getScreenshot(page);
 
   console.log("======== Log off ========");
   await browser.close();
