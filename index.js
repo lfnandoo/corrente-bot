486d0f711a54d0c4c901e8bac777f7a0b495de96
lfnandoo <devfernandoof@gmail.com>
2020-10-03 16:11:39 -0300

    feat: added colors to logs

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
+const colors = require("colors");
 
 const fileNames = new Array();
 const linkedinNames = new Array();
 
 (async () => {
-  console.log("======== If there is any typo, the bot will fail ========");
+  console.log(
+    "======== If there is any typo, the bot will fail ========".bold.red
+  );
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 100, height: 1000 });
@@ -16,14 +19,14 @@ const linkedinNames = new Array();
     `https://www.linkedin.com/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fin%2Fluizfernandoo&fromSignIn=true&trk=public_authwall_profile-login-link`
   );
 
-  const userNameOrEmail = prompt("Linkedin username or email: ");
+  const userNameOrEmail = prompt("Linkedin username or email: ".blue);
   await page.type("#username", userNameOrEmail);
 
-  const password = prompt("Linkedin password: ");
+  const password = prompt("Linkedin password: ".blue);
   await page.type("#password", password);
 
   await page.focus("button");
-  console.log("======== Login ========");
+  console.log("======== Login ========".bold.yellow);
   await page.$eval("button", (form) => form.click());
 
   await page.waitForNavigation();
@@ -31,29 +34,33 @@ const linkedinNames = new Array();
 
   await browser.close();
 
-  console.log("======== Creating PDF ========");
+  console.log("======== Creating PDF ========".bold.yellow);
   await createPDF();
 
-  console.log("======== Output text ========");
+  console.log("======== Output text ========".bold.yellow);
   console.log(" ");
   linkedinNames.map((name) => {
-    console.log(name);
+    console.log(name.green);
     console.log(" ");
   });
 
   console.log(" ");
-  console.log("======== Made by: https://github.com/lfnandoo ========");
+  console.log(
+    "======== Made by: https://github.com/lfnandoo ========".bold.green
+  );
 })();
 
 async function getScreenshots(page) {
-  const user = prompt("User profile URL (https:/www.lindekin.com/in/xxx): ");
-  const jobRole = prompt("User job role: ");
+  const user = prompt(
+    "User profile URL (https:/www.lindekin.com/in/xxx): ".blue
+  );
+  const jobRole = prompt("User job role: ".blue);
   await page.goto(`https://www.linkedin.com/in/${user}`);
   await page.addStyleTag({ path: "styles.css" });
 
   await page.waitForSelector(".pv-top-card");
   await page.waitForSelector(".inline.t-24");
-  console.log("======== Capturing screenshot ========");
+  console.log("======== Capturing screenshot ========".yellow);
   const div = await page.$(".pv-top-card");
   const li = await page.$(".inline.t-24");
   const name = await (await li.getProperty("innerText")).jsonValue();
@@ -62,14 +69,16 @@ async function getScreenshots(page) {
   await div.screenshot({
     path: `./screenshots/${user}.png`
   });
-  console.log("======== Screenshot saved ========");
+  console.log("======== Screenshot saved ========".green);
   fileNames.push(user);
 
-  const keepScreenshot = prompt("[y] for more screenshot's and [s] for stop: ");
+  const keepScreenshot = prompt(
+    "[y] for more screenshot's and [s] for stop: ".blue
+  );
   if (keepScreenshot === "y" || keepScreenshot === "Y") {
     await getScreenshots(page);
   } else {
-    return console.log("======== Closing browser ========");
+    return console.log("======== Closing browser ========".green);
   }
 }
 
@@ -90,5 +99,5 @@ async function createPDF() {
 
   doc.end();
 
-  return console.log("======== PDF Created ========");
+  return console.log("======== PDF Created ========".green);
 }
