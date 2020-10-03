77315975dc33af8f4f7514abd3e667f8f3776478
lfnandoo <devfernandoof@gmail.com>
2020-10-03 14:06:27 -0300

    feat: add images with array data

--- pdf.js
+++ pdf.js
@@ -1,6 +1,8 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
 
+const fileNames = ["danielfercruz", "example", "igor-rebolla"];
+
 function pdff() {
   const doc = new PDFDocument({
     size: [350, 350]
@@ -10,8 +12,10 @@ function pdff() {
 
   doc.text("Corrente bot!", 100, 100);
 
-  doc.addPage().image("screenshots/example.png", {
-    fit: [210, 290]
+  fileNames.map((fileName) => {
+    doc.addPage().image(`screenshots/${fileName}.png`, {
+      fit: [210, 290]
+    });
   });
 
   doc.end();
